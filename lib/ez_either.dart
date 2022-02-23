4f0a2fc899f3a4ac98e837bd25c11006552f530c
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-02-23 19:30:41 -0300

    first commit

---
+++ lib/ez_either.dart
@@ -0,0 +1,38 @@
+library ez_either;
+
+class Either<L, R> {
+  late final dynamic value;
+
+  Either([this.value]) {
+    _handleInvalidType();
+  }
+
+  bool isLeft() {
+    _handleInvalidType();
+    if (value.runtimeType == L) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isRight() {
+    _handleInvalidType();
+    if (value.runtimeType == R) {
+      return true;
+    }
+    return false;
+  }
+
+  bool get _invalidType => value.runtimeType != R && value.runtimeType != L;
+
+  void _handleInvalidType() {
+    if (_invalidType) {
+      throw Exception(
+        "Value not in declared types",
+      );
+    }
+  }
+}
+
+Either<L, R> right<L, R>(R value) => Either<L, R>(value);
+Either<L, R> left<L, R>(L value) => Either<L, R>(value);
---
+++ test/ez_either_test.dart
@@ -0,0 +1,123 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:ez_either/ez_either.dart';
+
+Either<Failure, String> callback(String id) {
+  return right("Daale");
+}
+
+class Failure {
+  final String message;
+
+  Failure(this.message);
+}
+
+main() {
+  test(
+    "Should throw an exception when type is not listed in cast declaration",
+    () {
+      expect(
+        () {
+          Either<Exception, String> sut = Either(0);
+        },
+        throwsA(
+          isA<Exception>(),
+        ),
+      );
+    },
+  );
+
+  test(
+    "Should return right value when type matches casting",
+    () {
+      final Either<Exception, String> sut = Either("This should work");
+
+      expect(
+        sut.isRight(),
+        equals(true),
+      );
+      expect(
+        sut.value,
+        isA<String>(),
+      );
+      expect(
+        sut.value,
+        equals("This should work"),
+      );
+    },
+  );
+
+  test(
+    "Should return right value when type matches casting",
+    () {
+      final Either<Exception, String> sut = Either("This should work");
+
+      expect(
+        sut.isRight(),
+        equals(true),
+      );
+      expect(
+        sut.value,
+        isA<String>(),
+      );
+      expect(
+        sut.value,
+        equals("This should work"),
+      );
+    },
+  );
+
+  test(
+    "Should return either from left() with correct type",
+    () {
+      final Either<Failure, String> sut = left(
+        Failure("Failure"),
+      );
+
+      expect(
+        sut.isLeft(),
+        equals(true),
+      );
+      expect(
+        sut.isRight(),
+        equals(false),
+      );
+      expect(
+        sut.value,
+        isA<Failure>(),
+      );
+      expect(
+        sut.value.message,
+        equals(
+          "Failure",
+        ),
+      );
+    },
+  );
+
+  test(
+    "Should return either from right() with correct type",
+    () {
+      final Either<Failure, String> sut = right("String");
+
+      expect(
+        sut.isLeft(),
+        equals(false),
+      );
+      expect(
+        sut.isRight(),
+        equals(true),
+      );
+      expect(
+        sut.value,
+        isA<String>(),
+      );
+      expect(
+        sut.value,
+        equals(
+          "String",
+        ),
+      );
+    },
+  );
+}
